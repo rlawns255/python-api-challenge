de2f836bdba54a52bdf016e9b6d499ae229f099c
June Kimm <rlawns255@gmail.com>
2023-02-07 23:10:15 -0600

    Final update

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8e2c4358f8990cde432d342282b02d66"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6ca7be10a5514dbf8aae3186bb29b743"
